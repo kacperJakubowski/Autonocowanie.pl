2b4785e6e607fa2bb6f7c77c3cc76f13d88208fc
KoderRoku <90681570+kacperJakubowski@users.noreply.github.com>
2022-09-14 06:16:12 +0200

    Delete content.js

--- Autonocowanie.pl/scripts/content.js
+++
@@ -1,6 +0,0 @@
-// document.querySelector(".js-wybor-sortowania").
-// let buttons = document.getElementsByClassName("tupper");
-// while (buttons.length != 0) {
-//   let url = buttons[0].href;
-//   window.open(url, "_blank");
-// }
